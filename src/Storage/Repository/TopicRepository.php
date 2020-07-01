c1a572b6243321b3c6f893a8da01a1b318b5efa5
alexdebril <alexdebril@users.noreply.github.com>
2020-07-01 22:52:34 +0200

    getTopics

--- src/Storage/Repository/TopicRepository.php
+++ src/Storage/Repository/TopicRepository.php
@@ -4,6 +4,7 @@ namespace FeedIo\Storage\Repository;
 
 use FeedIo\Storage\Entity\Topic;
 use MongoDB\BSON\ObjectIdInterface;
+use MongoDB\Driver\Cursor;
 use MongoDB\UpdateResult;
 
 class TopicRepository extends AbstractRepository
@@ -20,6 +21,13 @@ class TopicRepository extends AbstractRepository
         }
 
         return null;
+    }
+
+    public function getTopics(): Cursor
+    {
+        return $this->getCollection()->find([],
+            ['typeMap' => ['root' => Topic::class]]
+        );
     }
 
     public function save(Topic $topic): UpdateResult
